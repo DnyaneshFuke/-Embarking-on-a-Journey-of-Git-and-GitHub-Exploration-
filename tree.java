c31e1a072b7fa1de55b61821e1e9da365ab4daee
Dnyanesh <dnyaneshfuke1234@gmial.com>
2023-08-13 14:08:45 +0530

    now again commit after adding

---
+++ tree.java
@@ -0,0 +1,13 @@
+import java.Scanner.*;
+class tree
+{
+public static void main()
+{
+int sc = new Scanner(system.in());
+int r,pi=3.17;
+System.out.println("Enter the radius of circle");
+nextInt sc;
+
+}
+}
+
